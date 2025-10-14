5746f12da182c5b1cf87031446fdbdeacd04d8ab
ronald <kokomyriaque@gmail.com>
2025-10-14 15:52:12 +0100

    correction des bugs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true, // reflète l'origine de la requête (autorise toutes les origines)
+    credentials: true,
+  });
   await app.listen(3004);
 }
 bootstrap();
